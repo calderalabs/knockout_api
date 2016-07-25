b661a608956ed8b4efbd5a829e75035f693894c5
Matteo Depalo <matteodepalo@gmail.com>
2016-07-25 11:58:55 +0100

    Remove static manifest config

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,6 @@ use Mix.Config
 # which you typically run after static files are built.
 config :knockout_api, KnockoutApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   url: [scheme: "https", host: "knockout-api.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
