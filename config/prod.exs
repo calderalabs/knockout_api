b1f7b80f53dd67174c0d32a291ef95f5aba2e2d3
Matteo Depalo <matteodepalo@gmail.com>
2016-07-25 11:41:02 +0100

    Update production config files

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,16 @@ use Mix.Config
 # which you typically run after static files are built.
 config :knockout_api, KnockoutApi.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  url: [scheme: "https", host: "knockout-api.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
+config :knockout_api, KnockoutApi.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +66,3 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
